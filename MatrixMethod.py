35d6faa92333c1c9942776f7b068c4057c860a74
Anissa <mabed.anissa@gmail.com>
2022-01-18 17:27:41 +0100

    Méthode matricielle : vérification en parallèle avec le TD de ML

--- MatrixMethod.py
+++ MatrixMethod.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-#X = np.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 1, 1, 1, 1, 1], [1, 1, 0, 0, 0, 0, 0, 0, 1, 1]])
 G = np.array([[1, 0, 1], [1,0,1], [1,0,0], [1,0,0], [1,0,0], [1,1,0], [1,1,0], [1,1,0], [1,1,1], [1,1,1]])
 Pf1 = np.array([0.7,0.7,0.6,0.6,0.6,0.9,0.9,0.9,1,1])
 
@@ -11,9 +10,13 @@ G_nbraws = np.size(G, 0)
 Gd = np.c_[np.ones(G_nbraws), G]  # design matrix
 
 Gd_t = Gd.transpose()
+
+print("shape1 ", (np.linalg.pinv(Gd_t @ Gd) @ Gd_t).shape)
+print("shape2 ", (Gd_t @ Gd).shape)
+
 lam = np.linalg.pinv(Gd_t @ Gd) @ Gd_t @ Pf1
 print("lambda: {}".format(lam))
-#print("sol: {}".format(Gd @ theta))
-# prediction for any new vector
-#x = np.array([1.0, 8.0, 15.0])
-#print("prediction: {}".format(x @ theta))
+print("sol: {}".format(Gd @ lam))
+#prediction for any new vector
+x = np.array([1, 0, 0, 1])
+print("prediction: {}".format(x @ lam))
