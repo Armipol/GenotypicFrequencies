38c07fe56619db0f1ddae9909c1916a7a750b75f
Rémi <remi.prevot@mines-ales.org>
2022-02-22 15:01:34 +0100

    Mise au propre des codes

--- ExcelGeneratorReal.py
+++ ExcelGeneratorReal.py
@@ -1,99 +1,41 @@
-import random
-
-from scipy import stats
-import random as rd
 import pandas as pd
-import numpy as np
-from math import sqrt
 
 from FileReader import *
 
-def generation_reelle(G, reads, nb_iter):
-    for iter in range(nb_iter):
-        #freq = np.array(generateLambda(nb_genomes))
-        #G = generateGauto(nb_genomes,nb_snp)
-        #reads = generateReads_observ(nb_snp,freq,G)
-        # Pf1_attendu = G@freq
-        matrice = G.T
-        # Pf1_attendu_excel = np.append(Pf1_attendu,0)
-        # Matrice = np.vstack([Matrice,Pf1_attendu_excel])
-        reads_excel = np.c_[reads,[[0],[0]]]
-        matrice = np.vstack([matrice,reads])
-        print("matrice :", matrice[-1])
-        indiceLigne = []
-        indiceCol = []
-        for i in range(len(G[0])):
-            indL = 'G'+str(i+1)
-            indiceLigne.append(indL)
-        for j in range(len(G)):
-            indC = 'SNP'+str(j+1)
-            indiceCol.append(indC)
-        print("indice ligne", indiceLigne)
-        print("len indice colonne", len(indiceCol))
-        print("ind col -2", indiceCol[-2])
+## Filepaths
+filepath_positions = "D:/Rémi/Documents/IMT/3A/S10/EtudeTech/melange_simul_renom/positions_correspondance.txt"
+filepath_reads = "D:/Rémi/Documents/IMT/3A/S10/EtudeTech/melange_simul_renom/SIMULS_READS_MIXTURES_fauxBAM_fauxREADS/reads_statistics.txt"
+filepath_nucleotypes = "D:/Rémi/Documents/IMT/3A/S10/EtudeTech/melange_simul_renom/nucleotypes.txt"
+filepath_mixtures = "D:/Rémi/Documents/IMT/3A/S10/EtudeTech/melange_simul_renom/simulated_mixtures_composition.txt"
 
-        # indiceLigne.append('fq1 attendue')
-        indiceLigne.append('nbReads')
-        indiceLigne.append('nb1')
-        df = pd.DataFrame(matrice,index=indiceLigne,columns=indiceCol)
-        with pd.ExcelWriter('C:/Users/mabed/Documents/Travail/Etudes_techniques/resultats/output.xlsx', engine="openpyxl", mode="a") as writer:
-            df.to_excel(writer,sheet_name='N°'+str(iter+1))
-
-
-filepath_positions = "C:/Users/mabed/Documents/Travail/Etudes_techniques/fichiers_travail/positions_correspondance.txt"
-filepath_reads = "C:/Users/mabed/Documents/Travail/Etudes_techniques/fichiers_travail/reads_statistics.txt"
-filepath_nucleotypes = "C:/Users/mabed/Documents/Travail/Etudes_techniques/fichiers_travail/nucleotypes.txt"
-filepath_mixtures = "C:/Users/mabed/Documents/Travail/Etudes_techniques/fichiers_travail/simulated_mixtures_composition.txt"
-
+## Data
 data_utils = build_data_utils(filepath_positions, filepath_reads, filepath_nucleotypes, filepath_mixtures)
 mixtures_dict = data_utils[0]
 nucleotypes = data_utils[1]
 column_nucleotypes_dict = data_utils[2]
 positions_errors = data_utils[3]
 harp_dict = data_utils[4]
-
 mixtures_list = get_mixtures_list(mixtures_dict)
 
-G_test = generate_G_from_mix('Tm1001', mixtures_dict, nucleotypes, column_nucleotypes_dict)
-G_T_test = G_test.transpose()
-reads_test = reads_of_mix('Tm1001', harp_dict, positions_errors)
+## Creation titres
+indiceLigne = []
+indiceCol = []
+for i in range(12):
+    indL = 'G'+str(i+1)
+    indiceLigne.append(indL)
+indiceLigne.append('nbReads')
+indiceLigne.append('nb1')
+for j in range(4404):
+    indC = 'SNP'+str(j+1)
+    indiceCol.append(indC)
 
-
-generation_reelle(G_test, reads_test, 1)
-
-#ci-dessous, à inclure dans TraitementExcelStats
-#normalement j'ai pris les bonnes notations. À tester, notamment pas sûre du bon fonctionnement pour l'histogramme avec plt
-
-# from sklearn.metrics import mean_squared_error, mean_absolute_error, max_error
-# import matplotlib.pyplot as plt
-#
-# #Nelder-Mead
-# print("RMSE Nelder-Mead:", sqrt(mean_squared_error(freq, min_NM.x)))
-# print("MSE Nelder-Mead:", mean_squared_error(freq, min_NM.x))
-# print("MAE Nelder-Mead:", mean_absolute_error(freq, min_NM.x))
-# print("Max error Nelder-Mead:", max_error(freq, min_NM.x))
-#
-# plt.hist(sqrt(mean_squared_error(freq, min_NM.x)), bins=50)
-# plt.show()
-#
-# #Log Nelder-Mead
-# print("RMSE Log Nelder-Mead:", sqrt(mean_squared_error(freq, min_NMLog.x)))
-# print("MSE Log Nelder-Mead:", mean_squared_error(freq, min_NMLog.x))
-# print("MAE Log Nelder-Mead:", mean_absolute_error(freq, min_NMLog.x))
-# print("Max error Log Nelder-Mead:", max_error(freq, min_NMLog.x))
-#
-# plt.hist(sqrt(mean_squared_error(freq, min_NMLog.x)), bins=50)
-# plt.show()
-#
-# #TNC Log
-# print("RMSE TNC Log:", sqrt(mean_squared_error(freq, min_TNCLog.x.x)))
-# print("MSE TNC Log:", mean_squared_error(freq, min_TNCLog.x.x))
-# print("MAE TNC Log:", mean_absolute_error(freq, min_TNCLog.x.x))
-# print("Max error TNC Log:", max_error(freq, min_TNCLog.x.x))
-#
-# plt.hist(sqrt(mean_squared_error(freq, min_TNCLog.x.x)), bins=50)
-# plt.show()
-
-
-
-
+## Lancement génération du Excel
+for i in range(63,97):
+    print(i)
+    G_melange = generate_G_from_mix('Tm10'+str(i).zfill(2), mixtures_dict, nucleotypes, column_nucleotypes_dict)
+    reads_melange = reads_of_mix('Tm10'+str(i).zfill(2), harp_dict, positions_errors)
+    matrice = G_melange.T
+    matrice = np.vstack([matrice,reads_melange])
+    df = pd.DataFrame(matrice,index=indiceLigne,columns=indiceCol)
+    with pd.ExcelWriter('outputReel.xlsx', engine="openpyxl", mode="a") as writer:
+        df.to_excel(writer,sheet_name='N°'+str(i))
--- ExcelGeneratorSemiReal.py
+++ ExcelGeneratorSemiReal.py
@@ -5,11 +5,31 @@ import random as rd
 import pandas as pd
 import numpy as np
 
-import FileReader
+from FileReader import *
 
-positions_dict = FileReader.build_positions_dict("D:/Rémi/Documents/IMT/3A/S10/EtudeTech/melange_simul_renom/positions_correspondance.txt")
-harp_dict = FileReader.add_harp_positions("D:/Rémi/Documents/IMT/3A/S10/EtudeTech/melange_simul_renom/SIMULS_READS_MIXTURES_fauxBAM_fauxREADS/reads_statistics.txt", positions_dict)
-reads_possibles = FileReader.extract_reads_nb(harp_dict)
+filepath_positions = "D:/Rémi/Documents/IMT/3A/S10/EtudeTech/melange_simul_renom/positions_correspondance.txt"
+filepath_reads = "D:/Rémi/Documents/IMT/3A/S10/EtudeTech/melange_simul_renom/SIMULS_READS_MIXTURES_fauxBAM_fauxREADS/reads_statistics.txt"
+filepath_nucleotypes = "D:/Rémi/Documents/IMT/3A/S10/EtudeTech/melange_simul_renom/nucleotypes.txt"
+filepath_mixtures = "D:/Rémi/Documents/IMT/3A/S10/EtudeTech/melange_simul_renom/simulated_mixtures_composition.txt"
+
+data_utils = build_data_utils(filepath_positions, filepath_reads, filepath_nucleotypes, filepath_mixtures)
+mixtures_dict = data_utils[0]
+nucleotypes = data_utils[1]
+column_nucleotypes_dict = data_utils[2]
+positions_errors = data_utils[3]
+harp_dict = data_utils[4]
+mixtures_list = get_mixtures_list(mixtures_dict)
+
+# for mixture_name in mixtures_list:
+#     G = generate_G_from_mix(mixture_name, mixtures_dict, nucleotypes, column_nucleotypes_dict)
+#     print(G[0:10])
+#
+#     reads = reads_of_mix(mixture_name, harp_dict, positions_errors)
+#     print(reads)
+
+# positions_dict = build_positions_dict(filepath_positions)
+# harp_dict = add_harp_positions(filepath_reads, positions_dict)
+reads_possibles = extract_reads_nb(harp_dict)
 
 def generateLambda(nb_genotypes):
     freq_geno = np.random.random(size=nb_genotypes)
@@ -21,11 +41,14 @@ def generateLambda(nb_genotypes):
         freq_geno[1] -= 0.01
     return freq_geno
 
-def generateGauto(nb_genotypes,nb_snips):
-    return np.random.randint(2, size=(nb_snips,nb_genotypes))
+# def generateGauto(nb_genotypes,nb_snips):
+#     return np.random.randint(2, size=(nb_snips,nb_genotypes))
+G_test = generate_G_from_mix('Tm1002', mixtures_dict, nucleotypes, column_nucleotypes_dict)
+G_T_test = G_test.transpose()
 
-def generateReads_observ(nb_snips,freq,G):
-    reads_observ = np.array(random.choices(reads_possibles,k=nb_snips))
+
+def generateReads_observ(freq,G):
+    reads_observ = np.array(random.choices(reads_possibles,k=len(G)))
     Pf1 = G @ freq
     nb1_observe = np.random.binomial(reads_observ, Pf1)
     return np.vstack((reads_observ,nb1_observe))
@@ -37,11 +60,11 @@ def generateReads_observ(nb_snips,freq,G):
 #         x+=1
 #     return x
 
-def generationBasique(nb_genomes,nb_snp,nb_iter):
+def generationSemiReal(G,nb_iter):
     for iter in range(nb_iter):
-        freq = np.array(generateLambda(nb_genomes))
-        G = generateGauto(nb_genomes,nb_snp)
-        reads = generateReads_observ(nb_snp,freq,G)
+        freq = np.array(generateLambda(12))
+        # G = generateGauto(nb_genomes,nb_snp)
+        reads = generateReads_observ(freq,G)
         Pf1_attendu = G@freq
         Matrice = np.c_[G.T,freq]
         Pf1_attendu_excel = np.append(Pf1_attendu,0)
@@ -50,10 +73,10 @@ def generationBasique(nb_genomes,nb_snp,nb_iter):
         Matrice = np.vstack([Matrice,reads_excel])
         indiceLigne = []
         indiceCol = []
-        for i in range(nb_genomes):
+        for i in range(12):
             indL = 'G'+str(i+1)
             indiceLigne.append(indL)
-        for j in range(nb_snp):
+        for j in range(len(G)):
             indC = 'SNP'+str(j+1)
             indiceCol.append(indC)
         indiceLigne.append('fq1 attendue')
@@ -61,7 +84,7 @@ def generationBasique(nb_genomes,nb_snp,nb_iter):
         indiceLigne.append('nb1')
         indiceCol.append('freq_init')
         df = pd.DataFrame(Matrice,index=indiceLigne,columns=indiceCol)
-        with pd.ExcelWriter('output3.xlsx',mode='a') as writer:
+        with pd.ExcelWriter('outputSR.xlsx',mode='a') as writer:
             df.to_excel(writer,sheet_name='N°'+str(iter+1))
 
-generationBasique(12,5242,1)
+generationSemiReal(G_test,30)
